8ca1bc3e40249577e7f68ad69dd98d027d6d525f
Edoardo Sartori <dev@netawebs.net>
2025-09-28 22:53:44 +0200

    aa

---
+++ src/config/footer.js
@@ -0,0 +1,85 @@
+export const footerSocials = [
+  {
+    name: 'Twitter',
+    url: '',
+    icon: "mdi:twitter",
+  },
+  {
+    name: 'Youtube',
+    url: '',
+    icon: "mdi:youtube",
+  },
+  {
+    name: 'Whatsapp',
+    url: '',
+    icon: "mdi:whatsapp",
+  },
+  {
+    name: 'instagram',
+    url: '',
+    icon: "mdi:instagram",
+  },
+]
+
+export const footerLists = [
+	{
+		title: 'Servizi',
+		items: [
+			{
+				title: 'termoscanzioni',
+				slug: '/landing-pages/landing-1',
+			},
+			{
+				title: 'mappatura 3d',
+				slug: '/landing-pages/landing-2',
+			},
+			{
+				title: 'ispezioni aeree',
+				slug: '/landing-pages/landing-3',
+			},
+			{
+				title: 'concimazioni campi e servizi agricoli',
+				slug: '/landing-pages/landing-4',
+			},
+      // {
+			// 	title: 'Landing Page 3',
+			// 	slug: '/landing-pages/landing-3',
+			// },
+		],
+	},
+	{
+		title: 'recapiti',
+		items: [
+			{
+				title: 'p.iva:0000000000',
+				
+			},
+			{
+				title: 'mail: info@aaaa',
+				
+			},
+			{
+				title: 'cellulare: 0000000000',
+				
+			},
+		],
+	},
+	
+	{
+		title: 'informative',
+		items: [
+			{
+				title: 'chi siamo',
+				slug: '/company/about',
+			},
+			{
+				title: 'note legali',
+				slug: '/company/legal',
+			},
+			{
+				title: 'Contact',
+				slug: '/company/contact',
+			},
+		],
+	},
+];
---
+++ src/config/nav.js
@@ -0,0 +1,11 @@
+export const nav = [
+	{
+		title: 'Home',
+		slug: '/',
+	},
+	{
+		title: 'About',
+		slug: '/company/about',
+	},
+	
+];
