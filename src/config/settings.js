eefee92917823466e14fb6a88dd4ec9daaf94b97
tamagroup <u0044548101@gmail.com>
2025-10-17 16:29:37 +0200

    Update settings.js

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,5 +1,5 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
+	title: `SkyMetric`,
 	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
 	url: `https://odyssey-theme.sapling.supply`, // No trailing slash!
 	name: `SKY BASE`, // The short name of the business or brand name. Used for things like the copyright in the footer.
